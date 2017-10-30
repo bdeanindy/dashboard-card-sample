709b9897602b16df23422300776bb4e6603964ca
Benjamin Dean <ben@benjamindean.com>
2017-10-30 15:24:25 -0700

    Duh, can't operate without the Weebly middleware

---
+++ middleware/weebly.js
@@ -0,0 +1,33 @@
+"use strict";
+
+const express = require('express');
+
+/**
+ * Simple middleware which requires WEEBLY_CLIENT_ID
+ * and WEEBLY_SECRET_KEY to be set before continuing
+ *
+ * @param options
+ * @returns {Function}
+ * @constructor
+ */
+let WeeblyMiddleware = function(options) {
+	let clientId = process.env.MY_CLIENT_ID || options.client_id;
+	let secretKey = process.env.MY_CLIENT_SECRET || options.secret_key;
+
+	return function(req, res, next) {
+		if (!clientId) {
+			throw "Client ID must be defined";
+		}
+
+		if (!secretKey) {
+			throw "Secret Key must be defined";
+		}
+
+		req.app.clientId = clientId;
+		req.app.secretKey = secretKey;
+
+		next();
+	};
+};
+
+module.exports = WeeblyMiddleware;
