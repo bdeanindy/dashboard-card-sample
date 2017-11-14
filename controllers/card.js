3bd2a6590e9ca10ba290b24248cb5d2ae82fd9da
Benjamin Dean <ben@benjamindean.com>
2017-11-14 14:37:34 -0800

    WIP

--- controllers/card.js
+++ controllers/card.js
@@ -5,13 +5,11 @@ const request = require('request');
 const Utility = require('./utility');
 
 /**
- * Weebly Card API Interface.
+ * Weebly Card
  */
 
-const Card = module.exports = (
-    [weeblyApiBaseUri, clientId, clientSecret] = [process.env.WEEBLY_API_BASE_URI, process.env.WEEBLY_CLIENT_ID, process.env.WEEBLY_CLIENT_SECRET],
-
-    {}) => {
+const Card = module.exports = function(...options) {
+    // TODO: Bind options to this
 };
 
 // Vars
@@ -19,58 +17,59 @@ const weeblyAPI  = process.env.MY_API_BASE_URI;
 const clientId   = process.env.MY_CLIENT_ID;
 const secretKey  = process.env.MY_CLIENT_SECRET;
 
-// TODO: Rewrite this using spread operator and ES6
-Card.populateCard = function (params, cb) {
-    let token = params.token;
-    let userId = params.userId;
-    let siteId = params.siteId;
-    let cardId = params.cardId;
-    let userEmail = params.userEmail;
-    let testModeState = params.testModeState;
-    let loginLink = params.loginLink;
-    let sites = params.sites;
+Card.update = function (cardParams, cb) {
+    // Outbound Weebly Card API Request Arguments
+    const hMethod        = 'PATCH';
+    const hAccept        = 'application/vnd.weebly.v1+json';
+    const hContentType   = 'application/json';
+    const hToken         = cardParams.token;
 
-    let cardData = [
+    // Passthrough from the event data used in route below
+    let updateCardUrl   = `user/sites/${cardParams.site_id}/cards/${cardParams.platform_dashboard_card_id}`;
+    let token           = cardParams.token;
+    let userId          = cardParams.userId;
+    let siteId          = cardParams.siteId;
+    let cardId          = cardParams.cardId;
+
+    // Card Data - An array of dashboard componentsa (Array includes property values for each component on the card)
+    let updatedCardContent = [
         {
             "type": "text",
-            "title": userEmail,
-            "value": "User ID: " + userId
+            "title": "Dashboard Text Content",
+            "value": "Data for today..."
         },
+        {
+            "type": "group",
+            "label": "Nom Nom Data",
+            "components": [{
+                "type": "stat",
+                "label": "Clicks",
+                "primary_value": "0",
+                "primary_label": "Number of clicks on this component"
+                "link": "https://bdean-dashboard-card.herokuapp.com/cards/:jwt"
+            },
+            {
+                "type": "text",
+                "title": "Some text content",
+                "value": ""
+            }]
+        }
     ];
 
-    let siteList = {
-        "type": "group",
-        "label": "Sites",
-        "components": []
-    };
-
-    for(let i = 0; i < sites.length; i++) {
-        let siteObj = {
-            type: "link",
-            label: sites[i].title,
-            description: sites[i].domain,
-            link: sites[i].link
-        };
-        if(sites[i].publish_state) siteObj.value = "Published"
-        siteList.components.push(siteObj);
-    }
-
-    cardData.push(siteList);
-
-    let hash = Utility.generateHmac('PATCH' + '\n' + 'user/' + siteId + '\n' + JSON.stringify(cardData), secretKey)
     let payload = {
         card_id: cardId,
-        data: cardData,
+        card_data: updatedCardContent,
         site_id: siteId,
-        user_id: user
+        user_id: userId
     };
-    let reqOpts = {
-        method: 'PATCH',
-        url: this.weeblyAPI + 'user/sites/' + siteId + '/cards/' + cardId,
+
+    let reqOpt= {
+        method: reqMethod,
+        url: this.weeblyAPI + updateCardUrl,
         headers: {
-            'X-Weebly-Access-Token': token,
-            'Content-Type': 'application/json',
-            'Accepts': 'application/json'
+            'X-Weebly-Access-Token': hToken,
+            'Content-Type': hContentType,
+            'Accepts': hAccepts 
         },
     };
 
--- controllers/dashboard-cards-router.js
+++ controllers/dashboard-cards-router.js
@@ -10,11 +10,10 @@ const Card = require('./card');
  * Callback URL as specified in `manifest.json`
  */
 router.post('/callback', function(req, res) {
-	console.log(`\n`);
-	console.log(`POST request sent to '/webhooks/callback'`);
-	console.log(`\n`);
-	console.log(`Token: ${req.app.token}`);
-	console.log(`\n`);
+	//console.log(`POST request sent to '/webhooks/callback'`);
+	//console.log(`\n`);
+	//console.log(`Token: ${req.app.token}`);
+	//console.log(`\n`);
 	//console.log(`Request.SOMETHING: ${req}\n`);
 	let comparisonObject = {
 		'client_id': req.body.client_id,
@@ -28,56 +27,47 @@ router.post('/callback', function(req, res) {
 
 	let messages = [];
 
-	messages.push("\n");
-
-    // validate the hmac to see if its correct
+    // invalidate the hmac, is it correct?
 	if (!Utility.validateHmac(req.body.hmac, comparisonString, req.app.secretKey)) {
-		messages.push(`Fresh webhook was received, but the calculated hmac does not match what was passed.`);
-		messages.push(`Expected ${req.body.hmac}`);
-		messages.push(`Calculated ${Utility.generateHmac(comparisonString)}`);
+		//messages.push(`Fresh webhook was received, but the calculated hmac does not match what was passed.`);
+		//messages.push(`Expected ${req.body.hmac}`);
+		//messages.push(`Calculated ${Utility.generateHmac(comparisonString)}`);
 	} else {
-		messages.push('A fresh webhook was recieved:');
+		//messages.push('A fresh webhook was recieved:');
 	}
 
 	//{\n  "client_id": "465279841",\n  "client_version": "1.0.1",\n  "event": "dashboard.card.update",\n  "timestamp": 1503029424,\n  "data": {\n    "user_id": "108919051",\n    "site_id": "247763368794122525",\n    "platform_app_id": "465279841",\n    "platform_dashboard_card_id": "528166133822187070",\n    "platform_dashboard_card_version": "1.0.1",\n    "name": "devrel-interview",\n    "language": "en"\n  },\n  "hmac": "832035fade569f7a28ff27fcd16556d74d4e02cc396bcf3692ebd6727376f142"\n}
-	messages.push(`Headers: ${JSON.stringify(req.headers, null, 2)}`);
-	messages.push(`Data: ${JSON.stringify(req.body, null, 2)}`);
+	//messages.push(`Headers: ${JSON.stringify(req.headers, null, 2)}`);
+	//messages.push(`Data: ${JSON.stringify(req.body, null, 2)}`);
 
-	messages.push("\n");
+	//messages.push("\n");
 
 	// We only want to update the card data when we receive the appropriate events, in this case: `dasboard.card.update` events
 	if('dashboard.card.update' === req.body['event']) {
+		/*
 		messages.push(`Received update dashboard card event`);
 		messages.push("\n");
 		messages.push(`Full Request Body: ${req.body}`);
 		messages.push("\n");
 		messages.push(`Request Body Data Property: ${req.body.data}`);
 		messages.push("\n");
+		*/
 
-		let configObj = {};
-		// TODO: Bolt on the configObj properties from the event payload as context (so we update the correct dashboard card)
-
-		/*
-		Card.populateCard(function(err, data) {
+		Card.update(req.body.data, function(err, data) {
 			if(err) {
 				console.error(err, data);
 			} else {
-				//console.log('User Data: ', user);
+				console.log('User Data: ', data);
 				res.render('reseller', {
-					title: 'Manage Reseller User',
-					resellerId: '',
-					resellerTestMode: data.test_mode,
-					resellerEmail: data.email, 
-					resellerLanguage: data.language 
+					
 				});
 			}
 		});
-		*/
 	}
 
-	let message = messages.join("\n");
+	//let message = messages.join("\n");
+	//console.log('Messages: ', messages);
 
-	console.log('Messages: ', messages);
 	res.status(200).send(message);
 
 });
